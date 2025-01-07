2b6bf3991b22239ca451b23059f71ccfbd229752
ArshiAAkhavan <letmemakenewone@gmail.com>
2025-01-07 15:09:14 -0500

    hash-object

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::fs;
-use std::io::Read;
+use std::io::{BufReader, Read};
+use std::{fs, path::PathBuf};
 
 use anyhow::Ok;
 use clap::{Parser, Subcommand};
+use sha1::Digest;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -19,7 +20,11 @@ enum GitCmd {
         pretty_print: bool,
         hash: String,
     },
-    HashFile,
+    HashObject {
+        #[clap(short)]
+        write: bool,
+        path: String,
+    },
 }
 
 fn main() -> anyhow::Result<()> {
@@ -33,7 +38,7 @@ fn main() -> anyhow::Result<()> {
             anyhow::ensure!(pretty_print, "must pass -p flag");
             cat_file(&hash)
         }
-        GitCmd::HashFile => todo!(),
+        GitCmd::HashObject { write, path } => hash_object(write, &path),
     }
     Ok(())
 }
@@ -63,4 +68,34 @@ fn cat_file(file_hash: &str) {
     let data: Vec<u8> = iter.map(|c| *c).collect();
     let data = String::from_utf8(data).unwrap();
     print!("{data}")
+}
+
+fn hash_object(write: bool, path: &str) {
+    let mut object = std::fs::File::open(path).unwrap();
+
+    let mut buf = Vec::new();
+    let size = object.read_to_end(&mut buf).unwrap();
+    let header = format!("blob {size}\0");
+    let mut hasher = sha1::Sha1::new();
+    hasher.update(&header);
+    hasher.update(&buf);
+    let sha1sum = hasher.finalize();
+    let hex_string: String = sha1sum.iter().map(|byte| format!("{:02x}", byte)).collect();
+    print!("{hex_string}");
+
+    if !write {
+        return;
+    }
+    let mut object_path = PathBuf::from(".git/objects");
+    object_path.push(&hex_string[..2]);
+    fs::create_dir_all(&object_path).unwrap();
+
+    let mut blob = header.as_bytes().to_vec();
+    blob.extend(buf);
+    let mut zlib_encoder = flate2::read::ZlibEncoder::new(&blob[..], flate2::Compression::none());
+    let mut compressed_buf = Vec::new();
+    zlib_encoder.read_to_end(&mut compressed_buf).unwrap();
+
+    object_path.push(&hex_string[2..]);
+    fs::write(object_path, compressed_buf).unwrap();
 }
